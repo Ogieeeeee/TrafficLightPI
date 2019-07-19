4fd0fe62c2bd5e3bd4cccfe07d16cbc6c794aa5b
Oguzhan Kelleci <o.kelleci@live.nl>
2019-07-19 12:58:50 +0200

    Color counter LCD GROVEPI

--- TrafficlightAPI/Interfaces/IPIManager.cs
+++ TrafficlightAPI/Interfaces/IPIManager.cs
@@ -1,8 +1,10 @@
-﻿using Iot.Device.GrovePiDevice;
+﻿using Iot.Device.CharacterLcd;
+using Iot.Device.GrovePiDevice;
 using Iot.Device.GrovePiDevice.Sensors;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Device.I2c;
 using System.Linq;
 using System.Threading.Tasks;
 using TrafficlightAPI.Models;
@@ -11,14 +13,23 @@ namespace TrafficlightAPI.Interfaces
 {
     public interface IPIManager
     {
-        GrovePi grovePi {get;}
-        int pulse { get; set; }
+        GrovePi grovePi { get; }
+        int greenPulse { get; set; }
+        int redPulse { get; set; }
+        int orangePulse { get; set; }
+
+        I2cDevice i2cLcdDevice { get; }
+        I2cDevice i2cRgbDevice { get; }
+        LcdRgb1602 lcd { get; }
 
         List<string> GetList();
         string TurnLightOn(Colors color);
         string TurnLightOff(Colors color);
         int GetPulse();
+        int GetRedPulse();
+        int GetOrangePulse();
         int IncrementPulse();
+        void UpdateLCD();
 
     }
 }
--- TrafficlightAPI/Managers/PIManager.cs
+++ TrafficlightAPI/Managers/PIManager.cs
@@ -17,10 +17,12 @@ namespace TrafficlightAPI.Managers
 {
     public class PIManager : IPIManager
     {
-
         public List<string> Info;
-        public int pulse { get; set; }
+        public int redPulse { get; set; }
+        public int orangePulse { get; set; }
+        public int greenPulse { get; set; }
         I2cConnectionSettings i2CConnectionSettings;
+
 
         private GrovePi _grovePi;
         public GrovePi grovePi
@@ -35,17 +37,72 @@ namespace TrafficlightAPI.Managers
             }
         }
 
+        private I2cDevice _i2cLcdDevice;
+        public I2cDevice i2cLcdDevice
+        {
+            get
+            {
+                return _i2cLcdDevice;
+            }
+            private set
+            {
+                _i2cLcdDevice = value;
+            }
+        }
+
+        private I2cDevice _i2cRgbDevice;
+        public I2cDevice i2cRgbDevice
+        {
+            get
+            {
+                return _i2cRgbDevice;
+            }
+            private set
+            {
+                _i2cRgbDevice = value;
+            }
+        }
+
+        LcdRgb1602 _lcd;
+        public LcdRgb1602 lcd
+        {
+            get
+            {
+                return _lcd;
+            }
+            private set
+            {
+                _lcd = value;
+            }
+        }
+        
+
         public PIManager()
         {
             i2CConnectionSettings = new I2cConnectionSettings(1, GrovePi.DefaultI2cAddress);
             grovePi = new GrovePi(I2cDevice.Create(i2CConnectionSettings));
+
+            try
+            {
+                i2cLcdDevice = I2cDevice.Create(new I2cConnectionSettings(busId: 1, deviceAddress: 0x3E));
+                i2cRgbDevice = I2cDevice.Create(new I2cConnectionSettings(busId: 1, deviceAddress: 0x62));
+                lcd = new LcdRgb1602(i2cLcdDevice, i2cRgbDevice);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Lcd device not found.");
+            }
+
+
             Info = new List<string>();
-            pulse = 0;
+            greenPulse = 0;
+            orangePulse = 0;
+            redPulse = 0;
 
             grovePi.PinMode(GrovePort.DigitalPin4, PinMode.Output);
             grovePi.PinMode(GrovePort.DigitalPin3, PinMode.Output);
             grovePi.PinMode(GrovePort.DigitalPin2, PinMode.Output);
-
 
         }
 
@@ -70,14 +127,16 @@ namespace TrafficlightAPI.Managers
             else if (color == Colors.red)
             {
                 grovePi.DigitalWrite(GrovePort.DigitalPin3, PinValue.High);
+                IncrementRedPulse();
             }
             else if (color == Colors.yellow)
             {
                 grovePi.DigitalWrite(GrovePort.DigitalPin2, PinValue.High);
+                IncrementOrangePulse();
             }
 
-
-            return $"Succesfully turned {color} on, pulse = {pulse}";
+            UpdateLCD();
+            return $"Succesfully turned {color} on, pulse = {greenPulse}";
         }
 
         public string TurnLightOff(Colors color)
@@ -95,20 +154,63 @@ namespace TrafficlightAPI.Managers
                 grovePi.DigitalWrite(GrovePort.DigitalPin2, PinValue.Low);
             }
 
-
-            return $"Succesfully turned {color} off, pulse = {pulse}";
+            UpdateLCD();
+            return $"Succesfully turned {color} off, pulse = {greenPulse}";
         }
 
         public int GetPulse()
         {
-            return pulse;
+            return greenPulse;
         }
 
         public int IncrementPulse()
         {
-            pulse++;
-            return pulse;
+            greenPulse++;
+            return greenPulse;
         }
+
+        public int GetRedPulse()
+        {
+            return redPulse;
+        }
+
+
+        public int IncrementRedPulse()
+        {
+            redPulse++;
+            return redPulse;
+        }
+
+
+        public int GetOrangePulse()
+        {
+            return orangePulse;
+        }
+
+        public int IncrementOrangePulse()
+        {
+            orangePulse++;
+            return orangePulse;
+        }
+
+        public void UpdateLCD()
+        {
+            if (lcd != null)
+            {
+                lcd.Clear();
+                lcd.Write($"Red: {GetRedPulse()} Green: {GetPulse()}");
+
+                lcd.SetCursorPosition(0, 1);
+                lcd.Write($"Orange:{GetOrangePulse()}");
+
+            }
+            else
+            {
+                Console.WriteLine("No led device detected");
+            }
+
+        }
+
 
     }
 }
